cb9723d67113604e794dda8867af15490e949e08
Your NameAsadChowdhury020 <asadchowdhury020@gmail.com>
2026-06-01 13:10:34 +0600

    After finishing the crud operation

--- src/server.ts
+++ src/server.ts
@@ -76,6 +76,108 @@ app.get('/users', async(req : Request, res : Response) => {
     }
 })
 
+app.get('/users/:id', async ( req:Request, res : Response) => {
+    const { id } = req.params
+
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users WHERE id = $1
+            `,[id])
+
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                success : false,
+                message : "User not found",
+                data : {}
+            })
+        }
+        
+        res.status(200).json({
+            success : true,
+            message : "User retrived successfully!",
+            data : result.rows[0]
+            })
+    } catch (error : any) {
+        res.status(500).json({
+            success : false,
+            message : error.message,
+            error : error
+        })
+    }
+
+})
+
+app.put('/users/:id', async(req : Request, res : Response) => {
+    const { id } = req.params
+
+
+    try {
+        const { name, password, role } = req.body
+        const result = await pool.query(`
+            UPDATE users 
+            SET name = COALESCE( $1, name),
+            password = COALESCE($2, password),
+            role = COALESCE($3, role),
+            updated_at = NOW()
+
+            WHERE id = $4
+            RETURNING *
+            `,[name, password, role, id])
+
+        if(result.rows.length === 0){
+            return  res.status(404).json({
+                success : false,
+                message : "User not found!",
+                data : {}
+            })
+        }
+
+        res.status(200).json({
+            success : true,
+            message : "Users updated successfully!",
+            data : result.rows[0]
+            })
+    } catch (error : any) {
+        res.status(500).json({
+            success : false,
+            message : error.message,
+            error : error
+        })
+    }
+})
+
+app.delete('/users/:id', async(req : Request, res : Response) => {
+    const { id } = req.params
+
+    try {
+        const result = await pool.query(`
+            DELETE FROM users WHERE id = $1
+            `,[id])
+
+        if(result.rowCount === 0) {
+            return  res.status(404).json({
+                success : false,
+                message : "Users not found!",
+                data : {}
+            })
+        }
+
+         res.status(200).json({
+            success : true,
+            message : "Users deleted successfully!",
+            })
+        // console.log(result)
+    } catch (error : any) {
+        res.status(500).json({
+            success : false,
+            message : error.message,
+            error : error
+        })
+    }
+})
+
+
+
 app.get('/', (req : Request , res: Response ) => {
 //   res.send('Hello World!!')
 res.status(200).json({
